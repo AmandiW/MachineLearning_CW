60027b7c3a470e22ee900139d9a74bc0bb541581
AmandiW <amandi.20210983@iit.ac.lk>
2023-02-18 21:14:44 +0530

    Performed an Exploratory Data Analysis

---
+++ ExploratoryDataAnalysis.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the data into a pandas dataframe
+df = pd.read_csv("C:\\Users\\HP\\Downloads\\spambase\\spambase.data")
+
+# Performing a quick exploration
+print(df.head())
+print(df.info)
+print(df.describe())
+print(df.shape)
+
+# Check and count missing data and null values
+total_missing = df.isnull().sum().sum()
+total_null = df.isna().sum().sum()
+print("Total missing values: ", total_missing)
+print("Total null values: ", total_null)
