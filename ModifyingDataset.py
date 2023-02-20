09bbc9dc23b692e3649abb06e547a81452bac1eb
AmandiW <amandi.20210983@iit.ac.lk>
2023-02-20 11:55:54 +0530

    Added new files

---
+++ ModifyingDataset.py
@@ -0,0 +1,21 @@
+import re
+import pandas as pd
+
+# Read feature names from file and store in a list
+with open("C:\\Users\\HP\\Downloads\\spambase\\spambase.names") as spam:
+    text = spam.read()
+    feature_names = re.findall(r'\n(\w*_?\W?):', text)
+
+# Add "spam" column name to feature names list
+feature_names.append('spam')
+
+# Load the data from file using pandas, using feature names as column headers
+data = pd.read_csv("C:\\Users\\HP\\Downloads\\spambase\\spambase.data", header=None, names=feature_names)
+
+# Print the first few rows of the dataset to verify it was loaded correctly
+print(data.head())
+print(data.shape)
+
+# Save the updated dataset to a CSV file
+data.to_csv("C:\\Users\\HP\\Downloads\\MachineLearning_CW\\MachineLearning_CW\\ML_DatasetUpdated.csv", index=False)
+
