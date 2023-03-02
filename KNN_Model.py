ef3cd24879daaa648a9abfc800eb5e9dd30f1ad6
AmandiW <amandi.20210983@iit.ac.lk>
2023-03-02 16:42:22 +0530

    Updated KNN Model

--- KNN_Model.py
+++ KNN_Model.py
@@ -1,32 +1,37 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler, QuantileTransformer
 
-# Load the dataset
+# Load data
 data = pd.read_csv("ML_DatasetUpdated.csv")
-
 
 # Split the data into features and target variables
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Used a scaler to improve accuracy
+scaler = QuantileTransformer()
+X_scaled = scaler.fit_transform(X)
 
-# Create the KNN classifier
+# Perform PCA analysis. Used elbow method to find best n_components
+pca = PCA(n_components=10)
+X_pca = pca.fit_transform(X_scaled)
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=56)
+
+# Train KNN model
 knn = KNeighborsClassifier(n_neighbors=5)
-
-# Train the classifier on the training data
 knn.fit(X_train, y_train)
 
-# Make predictions on the testing data
 y_pred = knn.predict(X_test)
 
-# Calculate the accuracy of the classifier
 print(classification_report(y_test, y_pred))
-accuracy = accuracy_score(y_test, y_pred)
 
-print("Accuracy:", accuracy)
+# Calculate accuracy using accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print("KNN Model Accuracy =", accuracy)
